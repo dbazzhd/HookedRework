247e243a4b4ca1d4f488b72570bb7da1584e2638
Josh van Asten - Desktop <joshmathew.va@gmail.com>
2017-08-30 20:50:13 +0200

    + Readded the scripts folder and an EventManager as the folder is removed automatically if it contains nothing

---
+++ Hooked/Assets/Scripts/EventManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public static class EventManager
+{
+
+    private static Dictionary<string, UnityEvent> eventDictionary = new Dictionary<string, UnityEvent>();
+
+    public static void StartListening(string pEventName, UnityAction pListener)
+    {
+        UnityEvent newEvent = null;
+        if (eventDictionary.TryGetValue(pEventName, out newEvent))
+        {
+            newEvent.AddListener(pListener);
+        }
+        else
+        {
+            newEvent = new UnityEvent();
+            newEvent.AddListener(pListener);
+            eventDictionary.Add(pEventName, newEvent);
+        }
+    }
+
+    public static void StopListening(string pEventName, UnityAction pListener)
+    {
+        UnityEvent newEvent = null;
+        if (eventDictionary.TryGetValue(pEventName, out newEvent))
+        {
+            newEvent.RemoveListener(pListener);
+        }
+    }
+
+    public static void TriggerEvent(string pEventName)
+    {
+        UnityEvent newEvent = null;
+        if (eventDictionary.TryGetValue(pEventName, out newEvent))
+        {
+            newEvent.Invoke();
+        }
+    }
+}
